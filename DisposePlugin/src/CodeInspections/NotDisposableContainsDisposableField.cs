d8524effd863cdce8fd5947e62a33bce7c26a9d0
vorkulsky <vorkulsky@gmail.com>
2014-02-02 19:20:15 +0600

    NotDisposableContainsDisposableField

---
+++ DisposePlugin/src/CodeInspections/NotDisposableContainsDisposableField.cs
@@ -0,0 +1,37 @@
+﻿using JetBrains.ReSharper.Daemon;
+
+namespace DisposePlugin.CodeInspections
+{
+    [StaticSeverityHighlighting(Severity.WARNING, "CSharpInfo")]
+    internal class NotDisposableContainsDisposableField : IHighlighting
+    {
+        protected readonly string Message;
+
+        public NotDisposableContainsDisposableField(bool implementsDisposableInterface)
+        {
+            Message = implementsDisposableInterface
+                ? "Class contains disposable fiels, but not implements dispose method itself"
+                : "Class contains disposable fiels, but is not disposable itself";
+        }
+
+        public string ToolTip
+        {
+            get { return Message; }
+        }
+
+        public string ErrorStripeToolTip
+        {
+            get { return Message; }
+        }
+
+        public int NavigationOffsetPatch
+        {
+            get { return 0; }
+        }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+    }
+}
